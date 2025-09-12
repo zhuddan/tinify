51f178652e2b012a6526548be50e02fc0378d11d
zhuddan <zdecode@outlook.com>
2025-09-12 11:35:57 +0800

    1

--- src/utils/banner.ts
+++ src/utils/banner.ts
@@ -6,8 +6,8 @@ import { getVersionUpdateInfo } from './version'
 export function displayBanner(postinstall = false) {
   const bannerMessage = `${chalk.cyan(` ${name}@${version}\n`)}`
     + `${chalk.cyanBright('tinify --help')} ${chalk.gray('查看使用帮助')}`
-    + `${getVersionUpdateInfo()}\n`
-    + `${postinstall ? `${chalk.greenBright('✅ 安装成功')}` : ''}`
+    + `${getVersionUpdateInfo()}`
+    + `\n${postinstall ? `${chalk.greenBright('✅ 安装成功')}` : ''}`
   const box = boxen(bannerMessage, {
     title: '🚀',
     padding: { top: 1, bottom: 1, left: 15, right: 15 },
